875fa41ed8ecb5b8a947acaa6609c4cf238697c5
tprat <Titouan.p@hotmail.fr>
2024-07-04 17:14:05 +0200

    some error handle

--- controllers/galaxyController.js
+++ controllers/galaxyController.js
@@ -4,15 +4,19 @@ import crypto from 'crypto';
 
 export const getGalaxies = (req, res) => {
 	const galaxies = findGalaxies();
-	APIResponse(res, galaxies, "All galaxies");
+	galaxies ? APIResponse(res, galaxies, galaxies.length + " galaxies found") : APIResponse(res, null, "No galaxies found");
 };
 
 export const getGalaxyById = (req, res) => {
 	const galaxy = findGalaxyById(req.params.id);
-	APIResponse(res, galaxy, "Galaxy found");
+	galaxy ? APIResponse(res, galaxy, "Galaxy found") : APIResponse(res, null, "Galaxy not found");
 };
 
 export const addGalaxy = (req, res) => {
+	if (!req.body.name || !req.body.type || !req.body.age || !req.body.diameter) {
+		APIResponse(res, null, "Missing required fields", 400);
+		return;
+	}
 	const newGalaxy = {
 		id: crypto.randomUUID(),
 		...req.body,
