836015fb08ef9d4c194d954961f9a0b39283e136
tprat <Titouan.p@hotmail.fr>
2024-09-10 17:16:42 +0200

    add typescript init and db connexion

---
+++ migrations/0000_true_proteus.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS "galaxies" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"type" text NOT NULL,
+	"age" integer NOT NULL,
+	"diameter" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "planetary_systems" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"star" text NOT NULL,
+	"star_type" text NOT NULL,
+	"age" integer NOT NULL,
+	"galaxy_id" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "planets" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"type" text NOT NULL,
+	"distance_to_sun" integer NOT NULL,
+	"radius" integer NOT NULL,
+	"mass" integer NOT NULL,
+	"planetary_system_id" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "satellites" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"distance_to_planet" integer NOT NULL,
+	"radius" integer NOT NULL,
+	"planet_id" integer NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "planetary_systems" ADD CONSTRAINT "planetary_systems_galaxy_id_galaxies_id_fk" FOREIGN KEY ("galaxy_id") REFERENCES "public"."galaxies"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "planets" ADD CONSTRAINT "planets_planetary_system_id_planetary_systems_id_fk" FOREIGN KEY ("planetary_system_id") REFERENCES "public"."planetary_systems"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "satellites" ADD CONSTRAINT "satellites_planet_id_planets_id_fk" FOREIGN KEY ("planet_id") REFERENCES "public"."planets"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
